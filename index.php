c6b31d4a19663c5c4998455fa692ae73a430de43
Võ Huy Khang <47880923+huykhangvo@users.noreply.github.com>
2021-02-04 08:36:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,228 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>Trang chủ</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="description" content="Your site description" />
+
+    <link rel="icon" href="images/favicon.png" type="image/x-icon" />
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:700,300" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" type="text/css" href="css/normalize.min.css" />
+
+    <link rel="stylesheet" type="text/css" href="css/style.lite.min.css" />
+</head>
+
+<body id="parent">
+
+    <div class="main-container">
+
+        <div class="header">
+            <div class="clearfix">
+
+                <div class="float-left logo">
+                    <h1>
+                        <a href="index.php">
+                            <img src="images/logo.png" alt="" />
+                        </a>
+                    </h1>
+                </div>
+
+                <div class="float-right">
+                    <ul id="top-nav" class="slimmenu">
+
+                        <li>
+                            <a href="#" target="_parent">Giới Thiệu</a>
+                        </li>
+                        <li>
+                            <a href="#" target="_parent">Liên Hệ</a>
+                        </li>
+
+                        <li>
+                            <a href="#">Mạng xã hội</a>
+                            <ul>
+                                <li>
+                                    <a href="#">Facebook</a>
+                                </li>
+                                <li>
+                                    <a href="#">Zalo</a>
+                                </li>
+                                <li>
+                                    <a href="#">Telegram</a>
+                                </li>
+                                <li>
+                                    <a href="#">Instagram</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="#" class="fullscreen tooltip" title="Go Full Screen">
+                                <span class="pictogram">&#xe801;</span>
+                            </a>
+                            <a href="#" class="fullscreenExit tooltip" title="Exit Full Screen">
+                                <span class="pictogram">&#xe802;</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div id="freewall" class="free-wall">
+            <div class="brick tint size11 cat-2">
+                <img src="images/4538256461_96f9e70e9c_b.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">Meet.google</h3>
+                    <p class="project-description"> Học online
+                        <br><br>
+                        203_1KT1001_05
+                        <br><br>
+                        203_1CT1104_03
+                        <br><br>
+                        203_1CT1106_01
+                        <br>
+                    </p>
+                    <a href="https://meet.google.com" class="open-project" target="_parent">Vào lớp <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+            <div class="brick tint size11 cat-2">
+                <img src="images/3084871415_da49ab66ea_o.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">Thời Khóa Biểu</h3>
+                    <p class="project-description">Lịch học, thông báo, tin tức, điểm số,...</p>
+                    <a href="https://ems.vlute.edu.vn/account/login" class="open-project" target="_parent">Mở <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+
+            <div class="brick size11" data-fixpos="1-0">
+
+                <div class="cover">
+                    <p>
+                        <strong>Xin chào!</strong>
+                    </p>
+                    <p>Chào mừng bạn đến web</p>
+                    <p>Hãy tự do khám phá các đường dẫn của tôi.</p>
+                </div>
+            </div>
+            <div class="brick tint size12 cat-3">
+                <img src="images/4414454672_e91f887d34_o.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">Xem Phim</h3>
+                    <p class="project-description">Anime, Phim Hàn Quốc, Hành Động, Hài,...</p>
+                    <a href="https://www.netflix.com" class="open-project" target="_parent">Xem Phim<span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+            <div class="brick size11 cycle-slideshow client-number" data-cycle-fx="scrollHorz" data-cycle-speed="200" data-fixpos="1-2">
+                <img src="images/3346647884_a47c71fd9a_o.jpg" alt="" />
+                <img src="images/1452336320_483f721d3d_o.jpg" alt="" />
+                <img src="images/4538256461_96f9e70e9c_b.jpg" alt="" />
+                <img src="images/2310704408_361758fa28_b.jpg" alt="" />
+                <div class="client">
+                    <div class="number">365</div>
+                    <div class="client-text">Báo Mới</div>
+                    <div class="client-text">
+                        <a href="https://zingnews.vn/">
+                            <strong>Đọc Báo!</strong>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="brick tint size11 cat-3">
+                <img src="images/2310704408_361758fa28_b.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">Facebook</h3>
+                    <p class="project-description">Mạng Xã Hội</p>
+                    <a href="https://www.facebook.com" class="open-project" target="_parent">Mở <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+            <div class="brick tint size11 cat-1">
+                <img src="images/5171071313_e4c110ccb2_b.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">youtube</h3>
+                    <p class="project-description">YouTube là một nền tảng chia sẻ video phổ biến, là nơi người dùng có thể tải lên hoặc tải video về máy tính hay điện thoại và chia sẻ các video clip.</p>
+                    <a href="https://www.youtube.com/" class="open-project" target="_parent">Mở <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+
+            <div class="brick size11 cycle-slideshow" data-cycle-fx="fade" data-cycle-speed="900" data-cycle-slides="> div" data-fixpos="2-2">
+                <div class="cover">
+                    <h2>" Xã hội này chỉ có làm, chịu khó, cần cù thì bù siêng năng. Chỉ có làm thì mới có ăn. Những cái loại không làm mà đòi có ăn thì chỉ có ăn đầu b*i, ăn c*t."</h2>
+                    <p>Thầy Huấn</p>
+                </div>
+
+                <div class="cover">
+                    <h2>" Odipiscing aliquet. Nulla venenatis. In pede mi, aliquet sit amet, euismod in quis neque in neque sagittis scelerisque. Fusce quis quam mollis, varius diam at, tempus mi "</h2>
+                    <p>Vivamus dictum</p>
+                </div>
+
+
+
+            </div>
+            <div class="brick tint size11 cat-2">
+                <img src="images/4171682579_91f75f9a6a_o.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">Zing MP3</h3>
+                    <p class="project-description">Dịch vụ nhạc số với hàng triệu bài hát và MV có bản quyền chất lượng cao, giúp bạn nghe nhạc, tải nhạc, tải lên và đồng bộ kho nhạc của tôi trên nhiều thiết bị.</p>
+                    <a href="https://mp3.zing.vn/" class="open-project" target="_parent">Mở <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+            <div class="brick tint size11 cat-1">
+                <img src="images/1330348274_33c00c4886_o.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">tinhte.vn</h3>
+                    <p class="project-description">Tinhte.vn là mạng xã hội nghệ thuộc Công ty Cổ phần Mạng xã hội Tinh tế, hoặc còn gọi là Tinhte. Đây là một mạng xã hội Việt Nam chuyên về công nghệ, thiết bị tiêu dùng, điện gia dụng, thủ thuật, chia sẻ kinh nghiệm về cuộc sống.</p>
+                    <a href="https://tinhte.vn/" class="open-project" target="_parent">Mở <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+<!-- Thêm mới vòng lặp for -->
+            <div class="brick tint size11 cat-1">
+                <img src="images/lazada.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">Lazada</h3>
+                    <p class="project-description">Mua sắm online</p>
+                    <a href="https://www.lazada.vn/" class="open-project" target="_parent">Mua sắm <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+<!-- Kết thúc vòng lặp for -->
+<!-- Thêm mới vòng lặp for -->
+<div class="brick tint size11 cat-1">
+                <img src="images/tiki.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">TIKI</h3>
+                    <p class="project-description">Mua sắm online</p>
+                    <a href="https://tiki.vn/" class="open-project" target="_parent">Mua sắm <span class="pictogram">&#xe803;</span></a>
+                </div>
+            </div>
+<!-- Kết thúc vòng lặp for -->
+            <div class="brick tint size21 cat-3">
+                <img src="images/4613181397_78853da480_b.jpg" alt="" />
+                <div class="overlay">
+                    <h3 class="project-title">Ảnh Nền</h3>
+                    <p class="project-description">hình nền pc, Laptop, ...</p>
+                    <a href="https://www.pexels.com/vi-vn/tim-kiem/4k/" class="open-project" target="_parent">Mở <span class="pictogram">&#xe803;</span></a>
+                </div>
+
+
+
+
+                <div class="footer">
+                    <div class="float-right copyright">
+                        <p>Copyright &copy; 2021<a target="_parent" href="https://www.facebook.com/vohuykhang2000/">HuyKhang</a></p>
+                    </div>
+                </div>
+            </div>
+
+            <iframe id="_parent" name="_parent"></iframe>
+
+            <script type="text/javascript" src="js/jquery-1.10.2.min.js"></script>
+            <script type="text/javascript" src="js/freewall.min.js"></script>
+            <script type="text/javascript" src="js/jquery.cycle2.min.js"></script>
+            <script type="text/javascript" src="js/jquery.slimmenu.min.js"></script>
+            <script type="text/javascript" src="js/custom-script.min.js"></script>
+</body>
+
+</html>
